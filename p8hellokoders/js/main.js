c1288ead4be85db27bdc69a1c6e8fbdd187ef452
exeibier <xabierhh@gmail.com>
2020-03-04 14:58:53 -0600

    add js class

---
+++ p8hellokoders/js/main.js
@@ -0,0 +1,21 @@
+
+/*
+console.log("hello koders!!!")
+console.warn("aware koders!!!")
+console.error("error koders!!!")
+*/
+
+/*1.-Crear las variables 'x' y 'y'
+2.- asignarles variables un valor numérico.
+3.- imprimir la suma de x - y 
+4.- imprimir la resta.
+5.- imprimir la multiplicacion.
+6.- imprimir la division.*/
+
+var x = 5
+var y = 3
+
+console.log ('la suma de las variables es', x + y);
+console.log ('la resta de las variables es', x - y);
+console.log ('la multiplicacion de las variables es', x * y);
+console.log ('la resta de las variables es', x / y);
