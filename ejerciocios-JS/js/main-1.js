61c47cf183c824e3a29aadbae70304e8c527f98f
xabier hernandez <exeibier@MacBook-Pro-de-xabier.local>
2020-04-15 15:02:34 -0500

    [add delete]

--- ejerciocios-JS/js/main-1.js
+++ ejerciocios-JS/js/main-1.js
@@ -22,7 +22,7 @@ const printProducts = () => {
                 <p>${productItem}</p>
             </div>
             <div class="col-12 col-lg-4">
-                <button type="button" class="btn btn-primary">Delete</button>
+                <button type="button" class="btn btn-primary btn-delete" >Delete</button>
             </div>
         </div>
             `
@@ -32,3 +32,28 @@ const printProducts = () => {
 
 var saveProductBtn = document.getElementById("submit-buttom");
 saveProductBtn.addEventListener("click",saveProduct);
+
+//var saveProductKey
+const addAlert = () => {
+    alert('Vas a borrar este producto')
+}
+
+
+var deleteBtn = document.getElementsByClassName("btn-delete");
+deleteBtn.addEventListener("click",addAlert);
+
+
+/*const addDeleteButtonListener = () => {
+    var deleteButtons = document.getElementsByClassName("btn-primary");
+    console.log(deleteButtons)
+    var i;
+    for(i=0; i < deleteButtons.length; i++){
+        deleteButtons[i].addEventListener("click",(event) => {
+            let productIndex = event.target.dataset.productIndex
+            console.log(productIndex)
+            products.splice(productIndex,1);
+            printProducts();
+            addDeleteButtonListener();
+        })
+    }
+}*/
